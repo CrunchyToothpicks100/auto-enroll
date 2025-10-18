d558dc0e3ded0b67a0fe57733d0b4cbf51b62401
Timothy Rousseau <78456313+TimothyRou@users.noreply.github.com>
2025-10-18 10:38:29 -0500

    setup the pre virt

--- autoenroll/autoenroll/autoenroll/urls.py
+++ autoenroll/autoenroll/autoenroll/urls.py
@@ -19,7 +19,7 @@ from django.urls import include,path
 
 
 urlpatterns = [
-    path("polls/", include("polls.urls")),
+    path("virtualadvisor/", include("virtualadvisor.urls")),
     path('admin/', admin.site.urls),
 ]
 
