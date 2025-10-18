d737aa3af842ae0ad026bec7c6dacd862e7c96d8
Timothy Rousseau <78456313+TimothyRou@users.noreply.github.com>
2025-10-18 14:04:01 -0500

    save

--- django2/autoenroll/views.py
+++ django2/autoenroll/views.py
@@ -5,4 +5,4 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     # return HttpResponse("Hello, world. You're at the polls index.")
-    return render(request, 'autoenroll\thestart.html',)
+    return render(request, 'autoenroll\\thestart.html',)
